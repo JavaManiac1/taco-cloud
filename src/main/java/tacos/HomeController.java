bce38774450bc79ae7b827ddff11a640f28f8973
Damian <damian_joniec@interia.pl>
2019-10-16 00:16:33 +0200

    checkout commit

---
+++ src/main/java/tacos/HomeController.java
@@ -0,0 +1,16 @@
+package tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by user on 16.10.2019.
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
